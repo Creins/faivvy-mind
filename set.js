267198631efe04363575854a896d6821bb88d934
sir-bravin111 <mrbravin69@gmail.com>
2025-12-21 05:49:45 +0300

    Add files via upload

---
+++ set.js
@@ -0,0 +1,69 @@
+const fs = require('fs-extra');
+const { Sequelize } = require('sequelize');
+if (fs.existsSync('set.env'))
+    require('dotenv').config({ path: __dirname + '/set.env' });
+const path = require("path");
+const databasePath = path.join(__dirname, './database.db');
+const DATABASE_URL = process.env.DATABASE_URL === undefined
+    ? databasePath
+    : process.env.DATABASE_URL;
+module.exports = { session: process.env.SESSION_ID || 'ezra',
+    PREFIXE: process.env.PREFIX || "+",
+    GITHUB : process.env.GITHUB|| 'https://github.com/mr-X-force/LUCKY-MD-XFORCE',
+    OWNER_NAME : process.env.OWNER_NAME || "FrediEzra",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "255752593977",
+    DEV : process.env.DEV || "FrediEzra Tz",
+              
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "non",
+    AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
+    AUTO_REACT : process.env.AUTO_REACTION || "no", 
+    AUTO_STICKER : process.env.AUTO_STICKER || "no",
+    AUTO_SAVE_CONTACTS : process.env.AUTO_SAVE_CONTACTS || 'no',
+    URL: process.env.URL || "https://files.catbox.moe/1q3yrw.jpg",  
+    AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || 'no',              
+    CHAT_BOT: process.env.CHAT_BOT || "no",              
+    AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "no",
+    AUTO_BLOCK: process.env.AUTO_BLOCK || 'no', 
+    GCF: process.env.GROUP_HANDLE || 'no', 
+    AUTO_REPLY : process.env.GREET_MESSAGE || "no", 
+    AUTO_STICKER : process.env.AUTO_STICKER || "no", 
+    AUTO_STATUS_TEXT: process.env.AUTO_STATUS_TEXT || 'Your Status Seen By LUCKY-MD-XFORCE',   
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || 'no',
+    ANTI_BUG : process.env.ANTI_BUG || "no",
+    ANTI_MENTION_GROUP : process.env.ANTI_MENTION_GROUP || "on",
+    ANTI_TAG : process.env.ANTI_TAG || "on",
+    ANTI_BAD : process.env.ANTI_BAD || "on",
+    ANTI_SHARE_GROUP : process.env.ANTI_SHARE_GROUP || "on",
+    ANTI_LINK_GROUP : process.env.ANTI_LINK_GROUP || "on",
+    AUTO_BIO: process.env.AUTO_BIO || 'yes',       
+    ANTI_CALL_TEXT : process.env.ANTI_CALL_TEXT || '',             
+    GURL: process.env.GURL  || "https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f",
+    WEBSITE :process.env.GURL || "https://fredi-ai-site.vercel.app",
+    CAPTION : process.env.CAPTION || "LUCKY-MD-XFORCE",
+    BOT : process.env.BOT_NAME || 'LUCKY-MD-XFORCE',
+    MODE: process.env.PUBLIC_MODE || "no",              
+    TIMEZONE: process.env.TIMEZONE || "Africa/Nairobi", 
+    PM_PERMIT: process.env.PM_PERMIT || 'no',
+    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME || null,
+    HEROKU_API_KEY : process.env.HEROKU_API_KEY || null,
+    WARN_COUNT : process.env.WARN_COUNT || '5' ,
+    ETAT : process.env.PRESENCE || '1',
+    DP : process.env.STARTING_BOT_MESSAGE || "yes",
+    LUCKY_ADM : process.env.ANTI_DELETE_MESSAGES || 'no',
+    ANTI_DELETE_GROUP : process.env.ANTI_DELETE_GROUP || 'no',
+    ANTI_CALL: process.env.ANTI_CALL || 'yes', 
+    AUTO_REPLY : process.env.AUTO_REPLY || "no", 
+    AUDIO_REPLY : process.env.AUDIO_REPLY || 'yes', 
+    VOICE_CHATBOT_INBOX : process.env.VOICE_CHATBOT_INBOX || "no",
+    DATABASE_URL,
+    DATABASE: DATABASE_URL === databasePath
+        ? "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway" : "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway",
+   
+};
+let fichier = require.resolve(__filename);
+fs.watchFile(fichier, () => {
+    fs.unwatchFile(fichier);
+    console.log(`mise à jour ${__filename}`);
+    delete require.cache[fichier];
+    require(fichier);
+})
