0349f1ecbe665595b6af529f20ee06c6bdadd77c
sir-bravin111 <mrbravin69@gmail.com>
2025-12-21 06:36:51 +0300

    Update ans.js

--- plugins/ans.js
+++ plugins/ans.js
@@ -6,7 +6,7 @@ const { default: axios } = require('axios');
 
 
 
-ezra({nomCom:"lucky",reaction:"🤷",categorie:"Fredi-IA"},async(dest,zk,commandeOptions)=>{
+ezra({nomCom:"lucky",reaction:"🤷",categorie:"creins-IA"},async(dest,zk,commandeOptions)=>{
 
   const {repondre,ms,arg}=commandeOptions;
   
@@ -45,7 +45,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
 
 
 
-  ezra({ nomCom: "bing4", reaction: "🌀", categorie: "Fredi-IA" }, async (dest, zk, commandeOptions) => {
+  ezra({ nomCom: "bing4", reaction: "🌀", categorie: "creins-IA" }, async (dest, zk, commandeOptions) => {
     const { repondre, arg, ms } = commandeOptions;
   
     try {
@@ -58,7 +58,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
       const response = await axios.get(`http://api.maher-zubair.tech/ai/photoleap?q=${image}`);
       
       const data = response.data;
-      let caption = '*bing images by Fredie Tech*';
+      let caption = '*bing images by creins xmd Tech*';
       
       if (data.status == 200) {
         // Utiliser les données retournées par le service
@@ -73,7 +73,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
     }
   });
   
-  ezra({ nomCom: "ai2", reaction: "🌀", categorie: "Fredi-IA" }, async (dest, zk, commandeOptions) => {
+  ezra({ nomCom: "ai2", reaction: "🌀", categorie: "creins xmd-IA" }, async (dest, zk, commandeOptions) => {
     const { repondre, arg, ms } = commandeOptions;
   
     try {
@@ -98,7 +98,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
   });
 
 
-ezra({ nomCom: "gpt-all", reaction: "🌀", categorie: "Fredi-IA" }, async (dest, zk, commandeOptions) => {
+ezra({ nomCom: "gpt-all", reaction: "🌀", categorie: "creins xmd-IA" }, async (dest, zk, commandeOptions) => {
     const { repondre, arg, ms } = commandeOptions;
   
     try {
